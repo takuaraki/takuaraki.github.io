35a2f3e10264a3b394e8580ea51479afc7d9d806
takuaraki <takuaraki7@gmail.com>
2014-10-13 17:26:02 +0900

    modify effect_button.js

--- application/template/effect_button.js
+++ application/template/effect_button.js
@@ -67,104 +67,56 @@ $(document).ready(function(){
             localEffects.reverseRGB = true;
             OffButton('edge');
             OffButton('gyogan');
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOn',
-                id: selfid,
-                effect: 'reverse'
-            }));
         }else if(effect == 'edge'){
             efBtnEdge.attr("src", "effectButtonEdgeOn.png");
             localEffects.edge = true;
             OffButton('reverse');
             OffButton('gyogan');
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOn',
-                id: selfid,
-                effect: 'edge'
-            }));
         }else if(effect == 'gyogan'){
             efBtnGyogan.attr("src", "effectButtonGyoganOn.png");
             localEffects.gyogan = true;
             OffButton('reverse');
             OffButton('edge');
             OffButton('slim');
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOn',
-                id: selfid,
-                effect: 'gyogan'
-            }));
         }else if(effect == 'slim'){
             efBtnSlim.attr("src", "effectButtonSlimOn.png");
             localEffects.slim = true;
             OffButton('gyogan');
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOn',
-                id: selfid,
-                effect: 'slim'
-            }));
         }else if(effect == 'mirror'){
             efBtnMirror.attr("src", "effectButtonMirrorOn.png");
             localEffects.mirror = true;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOn',
-                id: selfid,
-                effect: 'mirror'
-            }));
         }
+        
+        webSocket.emit('message', JSON.stringify({
+            type: 'effectOn',
+            id: selfid,
+            effect: effect
+        }));
     }
     
     function OffButton(effect){
         if(effect == 'reverse'){
             efBtnReverse.attr("src", "effectButtonReverseOff.png");
             localEffects.reverseRGB = false;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOff',
-                id: selfid,
-                effect: 'reverse'
-            }));
         }else if(effect == 'edge'){
             efBtnEdge.attr("src", "effectButtonEdgeOff.png");
             localEffects.edge = false;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOff',
-                id: selfid,
-                effect: 'edge'
-            }));
         }else if(effect == 'gyogan'){
             efBtnGyogan.attr("src", "effectButtonGyoganOff.png");
             localEffects.gyogan = false;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOff',
-                id: selfid,
-                effect: 'gyogan'
-            }));
         }else if(effect == 'slim'){
             efBtnSlim.attr("src", "effectButtonSlimOff.png");
             localEffects.slim = false;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOff',
-                id: selfid,
-                effect: 'slim'
-            }));
         }else if(effect == 'mirror'){
             efBtnMirror.attr("src", "effectButtonMirrorOff.png");
             localEffects.mirror = false;
-            
-            webSocket.emit('message', JSON.stringify({
-                type: 'effectOff',
-                id: selfid,
-                effect: 'mirror'
-            }));
         }
+        
+        webSocket.emit('message', JSON.stringify({
+            type: 'effectOff',
+            id: selfid,
+            effect: effect
+        }));
     }
     
     
